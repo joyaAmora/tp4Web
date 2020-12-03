1d158d82ab5104a3cf46ff0b9d7c8af78a677ae4
Jade <0770907@cshawi.ca>
2020-12-03 11:36:13 -0500

    lab5 debut d'intégration layout

--- app/controllers/accueil_controller.rb
+++ app/controllers/accueil_controller.rb
@@ -1,8 +1,12 @@
 #Jade Phaneuf
 #2020-11-19
 class AccueilController < ApplicationController
-    before_action :authenticate_user!, except: [:presentation]
-    layout "application"
+    before_action :authenticate_user!, except: [:index]
+    layout "general"
+
+    def index
+        @recettes = Recette.all
+    end
 
     def presentation
         @recettes = Recette.all
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'accueil#presentation'
+  root 'accueil#index'
   get 'mesrecettes', to: 'accueil#mesrecettes'
   get 'recette/:id', to: 'recette#show_1'
 
